d4f8f69608277664af87cc53d10162eb172db570
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2017-10-20 16:37:34 -0700

    Add files via upload

--- scheduler.cpp
+++ scheduler.cpp
@@ -5,124 +5,203 @@
 
 
 
-//std::map<int,std::string> newschedule(){
-  //make new schedule
 
-  //input variables;
-  std::string day;
-  int input_time;
-  std::string activity;
-  std::string while_stopper;
-  bool checker = true;
-  std::string empty;
-  //"day" maps
-  std::map<int, std::string> monday;
-  std::map<int, std::string> tuesday;
-  std::map<int, std::string> wednesday;
+std::string input;
 
-
-  std::map<int, std::string> thursday;
-  std::map<int,std::string> friday;
-  std::map<int, std::string> saturday;
-  std::map<int, std::string> sunday;
-
-  std::cout << "Welcome to your new schedule!!!\n"
-  "Remeber input time as military_time\n"
-  /*
-  "Only do intervals for hours"
-  "Example of proper input 12-13"
-  "Time is in military time"
-  */
-  "Example 1 pm = 13 in military time"<<std::endl;
-  std::cin >> empty;
-
-  while(checker){
-  std::cout << "Day: ";
-  std::cin >> day;
-  std::cout << "Time: ";
-  std::cin >> input_time;
-  std::cout << "Activity: ";
-  std::cin >> activity;
-
-  if (day == "monday"){
-    monday[input_time] = activity;
-  }
-  else if (day == "tuesday"){
-    tuesday[input_time] = activity;
-  }
-  else if (day == "wednesday"){
-    wednesday[input_time] = activity;
-  }
-  else if (day == "thursday"){
-    thursday[input_time] = activity;
-  }
-  else if (day == "friday"){
-    friday[input_time] = activity;
-  }
-  else if (day == "saturday"){
-    saturday[input_time] = activity;
-  }
-  else if (day == "sunday"){
-    sunday[input_time] = activity;
-  }
-  else{
-    std::cout << "not a valid day"<<std::endl;
-  }
-  std::cout << "Continue?(Y or N)"<<std::endl;
-  std::cin >> while_stopper;
-  if (while_stopper == "N"){
-    checker = false;
-  }
-
-//end of while loop
-  }
-
-//end of newschedule()
-}
-
-
-
-
+std::string day;
+int input_time;
+std::string activity;
+std::string while_stopper;
+bool checker = true;
+std::string empty;
+//"day" maps
+std::map<int, std::string> monday;
+std::map<int, std::string> tuesday;
+std::map<int, std::string> wednesday;
+std::map<int, std::string> thursday;
+std::map<int,std::string> friday;
+std::map<int, std::string> saturday;
+std::map<int, std::string> sunday;
 
 
 
 
 int main(){
-std::string input;
-
-
-
-
-
-
-
-
-std::cout << "Greetings Welcome to your scheduler APP!"<<std::endl;
+std::cout << std::endl << std::endl;
+std::cout << "Salutations! Welcome to your scheduler app!"<<std::endl;
 std::cout << "What do you want to do?"<<std::endl;
-std::cout << "1. Make a new schedule"<<std::endl;
-std::cout << "2. Make Changes to Your Schedule"<<std::endl;
-std::cout << "3. Recieve advice about optimal study times"<<std::endl;
-std::cout << "4. View your schedule"<<std::endl;
+std::cout << "1. Add things to your schedule"<<std::endl;
+std::cout << "2. Recieve advice about optimal study times"<<std::endl;
+std::cout << "3. View your schedule"<<std::endl;
+std::cout << "4. Exit the program"<<std::endl;
 
 std::cin >> input;
 // to make a new schedule
+
 if (input == "1"){
-  std::cout << "ARE YOU SURE IF YOU HAVE AN EXISTING SCHEDULE THIS WILL DELETE YOUR CHANGES (Y OR N)"<<std::endl;
-  std::cin >> input;
-  if (input == "Y"|| input == "y"){
-    newschedule();
+    //make new schedule
+
+    //variable to make sure they chose the right thing in the menu
+    std::string right_menu_check;
+    std::cout << std::endl << std::endl;
+    std::cout << "make changes to your schedule here\n"
+    "input days as (m,t,w,r,f,s,su)\n"
+    "input time as military_time\n"
+    /*
+    "Only do intervals for hours"
+    "Example of proper input 12-13"
+    "Time is in military time"
+    */
+    "Example: 1 pm = 13 in military time"<<std::endl;
+    std::cout << "Do you want to make changes to your schedule?"<<std::endl;
+    std::cin >> right_menu_check;
+    if (right_menu_check == "Y" || right_menu_check == "y"){
+      std::cout << std::endl<<std::endl;
+
+      while(checker){
+      std::cout << "Day: ";
+      std::cin >> day;
+      std::cout << "Time: ";
+      std::cin >> input_time;
+      std::cout << "Activity: ";
+      std::cin >> activity;
+
+      if (day == "m"){
+        monday[input_time] = activity;
+      }
+      else if (day == "t"){
+        tuesday[input_time] = activity;
+      }
+      else if (day == "w"){
+        wednesday[input_time] = activity;
+      }
+      else if (day == "r"){
+        thursday[input_time] = activity;
+      }
+      else if (day == "f"){
+        friday[input_time] = activity;
+      }
+      else if (day == "s"){
+        saturday[input_time] = activity;
+      }
+      else if (day == "su"){
+        sunday[input_time] = activity;
+      }
+      else{
+        std::cout << "not a valid day"<<std::endl;
+      }
+      std::cout << "Continue?(Y or N)"<<std::endl;
+      std::cin >> while_stopper;
+      if (while_stopper == "N"|| while_stopper == "n"){
+        break;
+      }
+
+
+    //end of while loop
+      }
+
+  }
+  else{
+//end of option one
+main();
+}
+main();
+}
+
+// give helpful hints
+if (input == "2"){
+  std::string advice_input;
+  std::cout << "which day do you want free time advice for! (m,t,w,r,f,s,su) or exit"<<std::endl;
+  std::cin >> advice_input;
+  if (advice_input == "m"){
+    //FIXME put in nested if statements to give advice for different
+    if (monday[6] == ""){
+      std::cout << "It looks like you have some time in the morning to study! Happy Studying!"<<std::endl;
+    }
+    else{
+
+    }
+  }
+
+
+
+//end of hint option
+main();
+}
+
+//show schedule
+if (input == "3"){
+  std::string which_day;
+  std::cout << "which day schedule? (m,t,w,r,f,s,su)"<<std::endl<<std::endl;
+  std::cin >> which_day;
+
+  if (which_day == "m"){
+    std::cout << "MONDAY"<<std::endl;
+    for (int i = 0; i < monday.size(); ++i){
+      std::cout << i<<": ";
+      std::cout << monday[i]<<std::endl;
+  }
+}
+  else if (which_day == "t"){
+    std::cout << "TUESDAY"<<std::endl;
+    for (int i = 0; i < tuesday.size(); ++i){
+      std::cout << i<<": ";
+      std::cout << tuesday[i]<<std::endl;
+  }
+}
+  else if (which_day == "w"){
+    std::cout << "WEDNESDAY"<<std::endl;
+    for (int i = 0; i < wednesday.size(); ++i){
+      std::cout << i<<": ";
+      std::cout << wednesday[i]<<std::endl;
+  }
+}
+  else if (which_day == "r"){
+    std::cout << "THURSDAY"<<std::endl;
+    for (int i = 0; i < thursday.size(); ++i){
+      std::cout << i<<": ";
+      std::cout << thursday[i]<<std::endl;
+  }
+}
+
+  else if (which_day == "f"){
+    std::cout << "FRIDAY"<<std::endl;
+    for (int i = 0; i < friday.size(); ++i){
+      std::cout << i<<": ";
+      std::cout << friday[i]<<std::endl;
+  }
+}
+
+  else if (which_day == "s"){
+    std::cout << "SATURDAY"<<std::endl;
+    for (int i = 0; i < saturday.size(); ++i){
+      std::cout << i<<": ";
+      std::cout << saturday[i]<<std::endl;
+  }
+}
+
+
+  else if (which_day == "su"){
+    std::cout << "SUNDAY"<<std::endl;
+    for (int i = 0; i < sunday.size(); ++i){
+      std::cout << i<<": ";
+      std::cout << sunday[i]<<std::endl;
+  }
+}
+//end of option 3
+main();
+}
+
+if (input == "4"){
+  std::string end_input;
+  std::cout << "are you sure?(Y or N)"<<std::endl;
+  std::cin >> end_input;
+  if (end_input == "Y" || end_input == "y"){
+    return 0;
   }
   else{
     main();
   }
-}
-
-// changes for schedule
-if (input == "2"){
-  std::cout << "";
-}
-if (input == "3"){
-
 }
 
 
@@ -166,3 +245,7 @@ monday[23]
 //end of main()
 return 0;
 }
+/*
+developer notes finish the hints
+and finish with the see schedule function
+*/
